0eeb9c9b494448e9924f16ee345df35cf607e7cf
dsutj <darrylsutj@gmail.com>
2024-03-01 15:09:52 +1300

    Word functionality completed

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -8,6 +8,8 @@ public class Main {
     public static void main(String[] args) {
         //Initialising variables
         int lives = 5;
+        boolean complete = false;
+
         //For storing which letters the user has inputted already
         ArrayList<String> letters = new ArrayList<String>();
         ArrayList<String> words = getWords();
@@ -19,7 +21,40 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello! Please enter your name:");
         String name = input.next();
-        System.out.printf("Hello %s, Welcome to Hangman!", name);
+        System.out.printf("Hello %s, welcome to Hangman!%n", name);
+
+        while (! complete) {
+            while (! hidden.equals(word) || lives != 0) {
+                String answer = "null";
+                gameBoard(lives, hidden);
+                while (answer.length() != 1) {
+                    try {
+                        answer = input.next();
+                        System.out.println("Please enter a single character.");
+                    } catch (Exception e) {
+                        System.out.println("Invalid input.");
+                    }
+                }
+
+                char guess = answer.charAt(0);
+                if (word.indexOf(guess) != -1) {
+                    hidden = guess(guess, hidden, word);
+                    System.out.println("Correct!");
+                } else {
+                    System.out.println("Incorrect!");
+                    lives -= 1;
+                }
+            }
+
+            if (lives == 0) {
+                System.out.println("Bad luck! Game over!");
+                complete = tryAgain();
+            }   else {
+                System.out.println("Congratulations! The word was " + word);
+                complete = tryAgain();
+            }
+        }
+
     }
 
     //This function reads the text file in the project folder with 100k words in it
@@ -60,20 +95,40 @@ public class Main {
         System.out.printf("Lives: %d%n", lives);
         System.out.println(hidden);
         System.out.println("Enter your guess:");
-        Scanner input = new Scanner(System.in);
-        String guess = "null";
-        try {
-            while (guess.length() != 1) {
-                guess = input.next();
-                System.out.println("Please enter a single character.");
-            }
-        }
     }
 
     //This function takes in the user's guess
-    public static String guess(char guess, String hidden) {
-        hidden.replace('_', guess);
+    public static String guess(char guess, String hidden, String word) {
+        ArrayList<Integer> inds = new ArrayList<Integer>();
+        for (int i = 0;i < word.length();i++) {
+            if (guess == word.charAt(i)) {
+                inds.add(i);
+            }
+        }
+        char[] hidden_a = hidden.toCharArray();
+        for (int ind:inds) {
+            hidden_a[ind] = guess;
+        }
+        hidden = String.valueOf(hidden_a);
         return hidden;
+    }
+
+    //This function asks if the user wants to try again
+    public static boolean tryAgain() {
+        Scanner input = new Scanner(System.in);
+        String answer = input.next().toLowerCase();
+        char reply = answer.charAt(0);
+        System.out.println("Try again?: (Y/N");
+        while (reply != 'y' && reply != 'n') {
+            System.out.println("Please answer with a Y or N.");
+            answer = input.next().toLowerCase();
+            reply = answer.charAt(0);
+        }
+        if (reply == 'y') {
+            return false;
+        }
+        System.out.println("Thanks for playing!");
+        return true;
     }
 
 }
