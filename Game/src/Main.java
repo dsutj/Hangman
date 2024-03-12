4eb03e56b010c5cdfb70965bf4fa1d8393dda5e3
dsutj <darrylsutj@gmail.com>
2024-03-12 23:33:35 +1300

    Completed game functionality. Added a user class.

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -7,61 +7,129 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         //Initialising variables
-        int lives = 5;
         boolean complete = false;
-
+        User user = new User();
         //For storing which letters the user has inputted already
-        ArrayList<Character> letters = new ArrayList<Character>();
         ArrayList<String> words = getWords();
-        String word = randomWord(words);
-        assert word != null;
-        String hidden = "_".repeat(word.length());
+        String word;
+        String hidden;
 
         //Greeting to the user
         Scanner input = new Scanner(System.in);
         System.out.println("Hello! Please enter your name:");
-        String name = input.next();
-        System.out.printf("Hello %s, welcome to Hangman!%n", name);
+        //String name = input.next();
+        user.setName(input.next());
+        System.out.printf("Hello %s, welcome to Hangman!%n", user.name);
 
         while (! complete) {
-            while (! hidden.equals(word) && lives != 0) {
+            word = randomWord(words);
+            assert word != null;
+            hidden = "_".repeat(word.length());
+            user.newGame();
+            while (! hidden.equals(word) && user.lives != 0) {
                 String answer = "null";
-                gameBoard(lives, hidden);
+                gameBoard(user.lives, hidden);
 
                 while (true) {
                     while (answer.length() != 1) {
                         try {
                             answer = input.next();
+                            if (answer.length() != 1) {
                             System.out.println("Please enter a single character.");
+                            }
                         } catch (Exception e) {
                             System.out.println("Invalid input.");
                         }
                     }
-                    if (!letters.contains(answer.charAt(0))) {
+                    if (!user.containsLetter(answer.charAt(0))) {
                         break;
                     }   else {
                         System.out.println("Please input a new letter!");
+                        answer = "null";
                     }
                 }
 
-                char guess = answer.charAt(0);
-                letters.add(guess);
+                char guess = answer.toLowerCase().charAt(0);
+                user.addLetter(guess);
                 if (word.indexOf(guess) != -1) {
                     hidden = guess(guess, hidden, word);
                     System.out.println("Correct!");
                 } else {
                     System.out.println("Incorrect!");
-                    lives -= 1;
+                    user.lives -= 1;
                 }
             }
 
-            if (lives == 0) {
+            if (user.lives == 0) {
                 System.out.println("Bad luck! Game over! The word was " + word);
-                complete = tryAgain();
             }   else {
                 System.out.println("Congratulations! The word was " + word);
-                complete = tryAgain();
+                user.addWin();
             }
+
+            System.out.println(user.getWins());
+            System.out.println("Try again?: (Y/N)");
+            String answer = input.next().toLowerCase();
+            char reply = answer.charAt(0);
+            while (reply != 'y' && reply != 'n') {
+                System.out.println("Please answer with a Y or N.");
+                answer = input.next().toLowerCase();
+                reply = answer.charAt(0);
+            }
+
+            if (reply == 'n') {
+                System.out.println("Thanks for playing!");
+                complete = true;
+            }
+
+            System.out.println("New Game!");
+        }
+
+    }
+
+    public static class User {
+        public int difficulty = 0;
+        public String name = null;
+        public int wins = 0;
+        public int lives = 5;
+        public ArrayList<Character> letters = new ArrayList<Character>();
+        public User() {}
+
+        public void setLives(int lives) {
+            this.lives = lives;
+        }
+
+        public int getLives() {
+            return this.lives;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void addWin() {
+            this.wins += 1;
+        }
+
+        public String getWins() {
+            return String.format("You have %d wins!",this.wins);
+        }
+
+        public boolean containsLetter(char letter) {
+            return this.letters.contains(letter);
+        }
+
+        public void addLetter(char letter) {
+            this.letters.add(letter);
+        }
+
+        public void newGame() {
+            this.letters.clear();
+            this.lives = 5;
         }
 
     }
@@ -89,10 +157,12 @@ public class Main {
     public static String randomWord(ArrayList<String> words) {
         try {
             Random rand = new Random();
-            String word;
+            String word = "no";
+            while (word.length() <= 5) {
             int number = rand.nextInt(100000);
             word = words.get(number);
-            return word;
+            }
+            return word.toLowerCase();
         } catch (Exception e) {
             System.out.println("Cannot find text file!");
             return null;
@@ -122,22 +192,5 @@ public class Main {
         return hidden;
     }
 
-    //This function asks if the user wants to try again
-    public static boolean tryAgain() {
-        Scanner input = new Scanner(System.in);
-        String answer = input.next().toLowerCase();
-        char reply = answer.charAt(0);
-        System.out.println("Try again?: (Y/N");
-        while (reply != 'y' && reply != 'n') {
-            System.out.println("Please answer with a Y or N.");
-            answer = input.next().toLowerCase();
-            reply = answer.charAt(0);
-        }
-        if (reply == 'y') {
-            return false;
-        }
-        System.out.println("Thanks for playing!");
-        return true;
-    }
 
 }
